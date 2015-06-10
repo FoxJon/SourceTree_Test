3413c977d39f6e08368878d644ecbeb9ec6cee0e
Fox, Jon <Jon_Fox@pamperedchef.com>
2015-06-09 21:01:24 -0500

    new project

---
+++ TEMP/TEMP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TEMP
+//
+//  Created by Fox, Jon on 6/9/15.
+//  Copyright (c) 2015 Fox, Jon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
